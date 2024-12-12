c6f3b2d619743e4bff27ee836394f2569a9778df
marks <44593179+markses@users.noreply.github.com>
2024-12-13 01:42:31 +0800

    Update InfoText.tsx

--- src/components/Wrapped/InfoText.tsx
+++ src/components/Wrapped/InfoText.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-function InfoText({
+export const InfoText = ({
   children,
   className = "",
 }: {
   children: React.ReactNode;
   className?: string;
-}) {
+}) => {
   return (
     <p className={`text-zinc-600 text-xl font-medium ${className}`}>
       {children}
     </p>
   );
-}
-
-export default InfoText;
+};
